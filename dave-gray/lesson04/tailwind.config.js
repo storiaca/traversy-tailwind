6644ec2693c668606cb86eae0ab08f82391d1d54
Aleksandar Ristic <storiaca@gmail.com>
2022-12-02 23:28:00 +0100

    Lesson 04 add naimation and js

--- dave-gray/lesson04/tailwind.config.js
+++ dave-gray/lesson04/tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
-        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)'},
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': { 
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        }  
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
